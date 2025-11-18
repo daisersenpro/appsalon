24b7b298b173d2776a78517656ebbc5432f8f2b4
Anyelo Bustos <sen.programador@gmail.com>
2025-11-17 22:48:39 -0300

    Creación: login  formulario, enlaces y botón

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,1 +1,33 @@
-<h1>Login</h1>
+<h1 class="nombre-pagina">Login</h1>
+<p class="descripcion-pagina">Inicia sesión con tus datos</p>
+
+<form class="formulario" method="POST" action="/">
+    <div class="campo">
+        <label for="email">Email</label>
+        <input 
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Tu Email"
+        />
+    </div>
+
+    <div class="campo">
+        <label for="password">Password</label>
+        <input 
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Tu Password"
+        />
+    </div>
+
+    <input type="submit" class="boton" value="Iniciar Sesión" />
+
+</form>
+
+<div class="acciones">
+    <a href="/crear-cuenta">¿Aún no tienes una cuenta? Crear una</a>
+    <a href="/olvide">¿Olvidaste tu password?</a>
+
+</div>
