22e34775e90aca796f22e2332e045b8523e1f871
Anyelo Bustos <sen.programador@gmail.com>
2025-11-20 19:01:51 -0300

    Restaurar: vista crear-cuenta con formulario

---
+++ views/auth/crear-cuenta.php
@@ -0,0 +1,39 @@
+﻿<h1 class="nombre-pagina">Crear Cuenta</h1>
+<p class="descripcion-pagina">Llena el siguiente formulario para crear una cuenta</p>
+
+<form class="formulario" method="POST" action="/crear-cuenta">
+
+    <div class="campo">
+        <label for="nombre">Nombre</label>
+        <input type="text" id="nombre" name="nombre" placeholder="Tu Nombre">
+    </div>
+
+    <div class="campo">
+        <label for="nombre">Apellido</label>
+        <input type="text" id="apellido" name="apellido" placeholder="Tu Apellido">
+    </div>
+
+    <div class="campo">
+        <label for="telefono">Telefono</label>
+        <input type="tel" id="telefono" name="telefono" placeholder="Tu Telefono">
+    </div>
+
+     <div class="campo">
+        <label for="email">E-mail</label>
+        <input type="email" id="email" name="email" placeholder="Tu E-mail">
+    </div>
+
+     <div class="campo">
+        <label for="password">Password</label>
+        <input type="password" id="password" name="password" placeholder="Tu Password">
+    </div>
+
+    <input type="submit" class="boton" value="Crear Cuenta" />
+</form>
+
+<div class="acciones">
+
+    <a href="/">Ya tienes una cuenta? Iniciar Sesión</a>
+    <a href="/olvide">¿Olvidaste tu password?</a>
+
+</div>
