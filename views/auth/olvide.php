12cea8fdea2fffba3e0286ac361dbe1bf9a221b3
Anyelo Bustos <sen.programador@gmail.com>
2025-11-20 19:05:02 -0300

    Agregar: vista olvide (recuperar password)

---
+++ views/auth/olvide.php
@@ -0,0 +1,17 @@
+<h1 class="nombre-pagina">Recuperar Password</h1>
+<p class="descripcion-pagina">Ingresa tu email y te enviaremos instrucciones para cambiar tu password</p>
+
+<form class="formulario" method="POST" action="/olvide">
+
+    <div class="campo">
+        <label for="email">E-mail</label>
+        <input type="email" id="email" name="email" placeholder="Tu E-mail">
+    </div>
+
+    <input type="submit" class="boton" value="Enviar instrucciones" />
+</form>
+
+<div class="acciones">
+    <a href="/">¿Ya tienes una cuenta? Iniciar Sesión</a>
+    <a href="/crear-cuenta">¿Aún no tienes una cuenta?</a>
+</div>
