61029e3636dc4effbf88f99b8b7b8bd912d78411
Anyelo Bustos <sen.programador@gmail.com>
2025-11-21 17:57:35 -0300

    Soluciono use Model\Usuario

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -3,6 +3,7 @@
 namespace Controllers;
 
 use MVC\Router;
+use Model\Usuario;
 
 class LoginController {
     public static function login(Router $router) {
@@ -10,25 +11,35 @@ class LoginController {
         $router->render('auth/login');
       
     }
+
     public static function logout() {
         // Lógica de cierre de sesión
         echo "Cerrando sesión";
     }
+
     public static function olvide(Router $router) {
         // Mostrar formulario de recuperación de contraseña
         $router->render('auth/olvide');
     }
+
     public static function recuperar() {
         // Lógica para restablecer la contraseña
         echo "Página para recuperar la contraseña";
     }
+
     public static function crear(Router $router) {
-        // Lógica para crear una cuenta
+    // Lógica para crear una cuenta
+
+         $usuario = new Usuario;
+        
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+            $usuario->sincronizar($_POST);
             
-            $usuario = $_POST['usuario'];
         }
 
-        $router->render('auth/crear-cuenta');
+        $router->render('auth/crear-cuenta', [
+            'usuario' => $usuario
+        ]);
     }
 }
