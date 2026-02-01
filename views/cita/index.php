da6250fd6daaa957791814e8e7407b20afe8f067
Anyelo Bustos <sen.programador@gmail.com>
2026-02-01 18:54:35 -0300

    Creación de Cita Controll e index iniciado

---
+++ views/cita/index.php
@@ -0,0 +1,20 @@
+<h1 class="nombre-pagina">Crear Nueva Cita</h1>
+
+<p class="descripcion-pagina">Elije tus servicios y proporciona tus datos</p>
+
+<div id="app">
+    <div id="paso-1" class="seccion">
+        <h2>Servicios</h2>
+        <p>Elije tus servicios a continuación</p>
+        <div id="servicios" class="listado-servicios"></div>
+    </div>
+    <div id="paso-2" class="seccion">
+        <h2>Tus Datos y Cita</h2>
+        <p>Coloca tus datos y fecha de la cita</p>
+    </div>
+    <div id="paso-3" class="seccion">
+        <h2>Resumen</h2>
+        <p>Verifica que la información sea correcta</p>
+    </div>
+
+</div>
